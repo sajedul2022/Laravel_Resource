166e747016eb916b30bd98bb3e9fd7b36445c3e8
sajedul2022 <sajedul.idb.info@gmail.com>
2023-02-13 19:06:44 +0600

    vue integrate in axios laravel

--- wdpf_51_laravel_pr2/app/Http/Controllers/Api/SearchController.php
+++ wdpf_51_laravel_pr2/app/Http/Controllers/Api/SearchController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SearchController extends Controller
 {
     public function search(Request $request){
-        return $request->all();
+        // return $request->all();
+
+         $term =  $request->item;
+
+        $result = DB::table('products')
+        ->where('product_name', 'like', "%$term%")->get();
+        return $result;
     }
 }
+
